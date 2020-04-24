dcf258acb6f41095d26eb8a5b36386974a39421c
Fernando <ferki30@live.com>
2020-04-24 20:44:43 +0200

    Diseño ventana perfil de usuario

--- UpBeat/src/app/cuenta/cuenta.component.ts
+++ UpBeat/src/app/cuenta/cuenta.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
+import { Usuario } from '../MODELO/Usuario';
+import { ServiceService } from '../Service/service.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cuenta',
@@ -7,9 +13,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CuentaComponent implements OnInit {
 
-  constructor() { }
+  href: string;
+  usuario: Usuario = new Usuario();
+  constructor(private breakpointObserver: BreakpointObserver, private router:Router, private service:ServiceService) { }
 
   ngOnInit(): void {
   }
 
+  actualizarRuta(ruta: string){
+    this.href = ruta;
+  }
+
+  getNombreUsuario(){
+    this.usuario = this.service.getUserLoggedIn();
+    return this.usuario.nombre;
+  }
 }
