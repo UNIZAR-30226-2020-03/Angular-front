1c75bb1eb3c9f25bf87cc48eea6508bdb724d05f
Sergio <738233@unizar.es>
2020-05-11 22:19:43 +0200

    Proyecto exportado

--- UpBeat/src/environments/environment.prod.ts
+++ UpBeat/src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCNmZ8cUHOKFRx1SQdr_w46yHUvsEzW464",
+    authDomain: "songstorage-538a9.firebaseapp.com",
+    databaseURL: "https://songstorage-538a9.firebaseio.com",
+    projectId: "songstorage-538a9",
+    storageBucket: "songstorage-538a9.appspot.com",
+    messagingSenderId: "483089646900",
+    appId: "1:483089646900:web:b671394937b1d52429d161",
+    measurementId: "G-LNGHZKR9PR"
+  }
 };
