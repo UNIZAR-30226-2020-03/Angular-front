4398c0ae2a16da37b98bd181eb795ac265f39ae0
Fernando <ferki30@live.com>
2020-05-26 18:59:25 +0200

    albumes con imagen implementado

--- UpBeat/src/app/albumes-menu/albumes-menu.component.ts
+++ UpBeat/src/app/albumes-menu/albumes-menu.component.ts
@@ -5,6 +5,9 @@ import { MatDialog } from '@angular/material/dialog';
 import { Album } from '../MODELO/Album';
 import { Artista } from '../MODELO/Artista';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { FirebaseStorageService } from '../firebase-storage.service'
+import { finalize } from 'rxjs/operators';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-albumes-menu',
@@ -69,13 +72,26 @@ export class AlbumesMenuComponent implements OnInit {
 })
 export class popUp2 {
 
-  constructor(private router:Router ,private service:ServiceService,public dialog: MatDialog,private _snackBar: MatSnackBar) { }
+  public mensajeArchivo = 'No hay un archivo seleccionado';
+  public datosFormulario = new FormData();
+  public nombreArchivo = '';
+  public URLPublica = '';
+  public porcentaje = 0;
+  public finalizado = false;
+
+  constructor(private firebaseStorage:FirebaseStorageService ,private service:ServiceService,public dialog: MatDialog,private _snackBar: MatSnackBar) { }
 
   album : Album = new Album();
   idAlbum : String;
+  public archivoForm = new FormGroup({
+    archivo: new FormControl(null, Validators.required),
+  });
 
   crearAlbum(){
     if (this.album.nombre != null){
+      if(this.album.pathImg == null){
+        this.album.pathImg="assets/PlaylistDefecto.jpeg"
+      }
       this.service.obtenerIdAlbum(this.album).subscribe(data =>{
         this.idAlbum = data["id"];
         this.crearAlbumAux(this.idAlbum);
@@ -98,6 +114,44 @@ export class popUp2 {
   openSnackBar(message: string, action: string) {
     this._snackBar.open(message, action, {
       duration: 2000,
+    });
+  }
+
+  public cambioArchivo(event) {
+    if (event.target.files.length > 0) {
+      for (let i = 0; i < event.target.files.length; i++) {
+        this.mensajeArchivo = `Archivo preparado: ${event.target.files[i].name}`;
+        this.nombreArchivo = event.target.files[i].name;
+        this.datosFormulario.delete('archivo');
+        this.datosFormulario.append('archivo', event.target.files[i], event.target.files[i].name)
+      }
+    } else {
+      this.mensajeArchivo = 'No hay un archivo seleccionado';
+    }
+  }
+  public subirArchivo() {
+    let archivo = this.datosFormulario.get('archivo');
+    let tarea = this.firebaseStorage.tareaCloudStorage(this.nombreArchivo, archivo);
+
+    //Cambia el porcentaje
+    tarea.percentageChanges().subscribe((porcentaje) => {
+      this.porcentaje = Math.round(porcentaje);
+      if (this.porcentaje == 100) {
+        this.finalizado = true;
+      }
+    });
+
+    tarea.snapshotChanges().pipe(
+      finalize(() => this.obtenerURL())
+    )
+    .subscribe()
+  }
+
+  obtenerURL(){
+    let referencia = this.firebaseStorage.referenciaCloudStorage(this.nombreArchivo);
+    referencia.getDownloadURL().subscribe((URL) => {
+      this.URLPublica = URL;
+      this.album.pathImg = this.URLPublica;
     });
   }
 
