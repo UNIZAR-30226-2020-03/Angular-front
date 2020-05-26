0385c47fc79df4ab88b8fb2bc1739ec741521528
Fernando <ferki30@live.com>
2020-05-26 18:11:46 +0200

    playlists con imagen implementado

--- UpBeat/src/app/playlists-menu/playlists-menu.component.ts
+++ UpBeat/src/app/playlists-menu/playlists-menu.component.ts
@@ -5,6 +5,11 @@ import { Router } from '@angular/router';
 import { MatDialog } from '@angular/material/dialog';
 import { Playlist } from '../MODELO/Playlist';
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { FirebaseStorageService } from '../firebase-storage.service'
+import { finalize } from 'rxjs/operators';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+
+
 
 @Component({
   selector: 'app-playlists-menu',
@@ -71,13 +76,39 @@ export class PlaylistsMenuComponent implements OnInit {
 })
 export class popUp {
 
-  constructor(private router:Router ,private service:ServiceService,public dialog: MatDialog,private _snackBar: MatSnackBar) { }
+  public mensajeArchivo = 'No hay un archivo seleccionado';
+  public datosFormulario = new FormData();
+  public nombreArchivo = '';
+  public URLPublica = '';
+  public porcentaje = 0;
+  public finalizado = false;
+
+  constructor(private firebaseStorage:FirebaseStorageService ,private service:ServiceService,public dialog: MatDialog,private _snackBar: MatSnackBar) { }
 
   playlist : Playlist = new Playlist();
   idPlaylist : String;
+  public archivoForm = new FormGroup({
+    archivo: new FormControl(null, Validators.required),
+  });
+
+  public cambioArchivo(event) {
+    if (event.target.files.length > 0) {
+      for (let i = 0; i < event.target.files.length; i++) {
+        this.mensajeArchivo = `Archivo preparado: ${event.target.files[i].name}`;
+        this.nombreArchivo = event.target.files[i].name;
+        this.datosFormulario.delete('archivo');
+        this.datosFormulario.append('archivo', event.target.files[i], event.target.files[i].name)
+      }
+    } else {
+      this.mensajeArchivo = 'No hay un archivo seleccionado';
+    }
+  }
 
   crearPlaylist(){
     if (this.playlist.nombre != null){
+      if(this.playlist.pathImg == null){
+        this.playlist.pathImg="assets/PlaylistDefecto.jpeg"
+      }
       this.service.obtenerIdPlaylist(this.playlist).subscribe(data =>{
         this.idPlaylist = data["id"];
         this.crearPlaylistAux(this.idPlaylist);
@@ -102,4 +133,31 @@ export class popUp {
       duration: 2000,
     });
   }
+
+  public subirArchivo() {
+    let archivo = this.datosFormulario.get('archivo');
+    let tarea = this.firebaseStorage.tareaCloudStorage(this.nombreArchivo, archivo);
+
+    //Cambia el porcentaje
+    tarea.percentageChanges().subscribe((porcentaje) => {
+      this.porcentaje = Math.round(porcentaje);
+      if (this.porcentaje == 100) {
+        this.finalizado = true;
+      }
+    });
+
+    tarea.snapshotChanges().pipe(
+      finalize(() => this.obtenerURL())
+    )
+    .subscribe()
+  }
+
+  obtenerURL(){
+    let referencia = this.firebaseStorage.referenciaCloudStorage(this.nombreArchivo);
+    referencia.getDownloadURL().subscribe((URL) => {
+      this.URLPublica = URL;
+      this.playlist.pathImg = this.URLPublica;
+    });
+  }
+
 }
